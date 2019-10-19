37b19ee7c7790b04f5197d40f58b541da7560fc1
Liuxiaokun <18729673011@163.com>
2019-10-19 23:45:23 +0800

    test

--- day01/test.py
+++ day01/test.py
@@ -16,14 +16,18 @@ l2 = [1,2,3,4]
 l2.sort()
 print(l2)
 print(type(l2))
+
 # set
 l3 = {1,2,3,4}
-
-# dict
+s = set([1,2,3,4])
 print(type(l3))
+print(type(s))
+# dict
 d = {'name':'tom','age':18}
 print(d)
 print(type(d))
-# set
-s = set([1,2,3,4])
-print(type(s))
+
+
+for i in s:
+    print(i)
+
