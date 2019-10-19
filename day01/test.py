2ca505976909429fc6480fd159a17553a2afe212
Liuxiaokun <18729673011@163.com>
2019-10-19 23:32:33 +0800

    test

--- day01/test.py
+++ day01/test.py
@@ -8,9 +8,22 @@ __author__ = 'xiaokun·liu'
 # 注释1
 print('...')  # 注释2
 
+# tuple
 l1 = (1,2,3,4)
 print(type(l1))
-l2 = ['1',2,3,4]
+# list
+l2 = [1,2,3,4]
+l2.sort()
+print(l2)
 print(type(l2))
+# set
 l3 = {1,2,3,4}
+
+# dict
 print(type(l3))
+d = {'name':'tom','age':18}
+print(d)
+print(type(d))
+# set
+s = set([1,2,3,4])
+print(type(s))
